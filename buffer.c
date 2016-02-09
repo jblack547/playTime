4c2d1686ba17564788a4a42d7111579430d80a29
Jim Black <jjblack@cardinalpeak.com>
2016-02-09 10:52:25 -0700

    preserve just the allocation function

--- buffer.c
+++ buffer.c
@@ -2,35 +2,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-char *data = NULL;
-
-int main(void)
-{
-   
-   wtf(&data);
-   printf("%s\n", data);
-   free(data);
-
-//   char buffer[256];
-//   make_buffer(buffer);
-//   printf("Buffer String is %s\n", buffer);
-
-   return (0);
-}
-
-#if 0
-int make_buffer(char *buf)
-{
-
-   char *fname = "TheFileName.asu";
-   printf("String length : %d pathname length: %d\n", strlen(fname), strlen("/cache/recovery"));
-   snprintf(buf, (strlen(fname)+strlen("/cache/recovery/")+1), "/cache/recovery/%s", fname);
-
-
-   return (0);
-
-}
-#endif
 
 
 int wtf(char **buf)
